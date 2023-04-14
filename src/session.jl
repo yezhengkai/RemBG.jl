b57aeeb74ce59155ac8191cf85677ec51396a42c
kai <supon3060@gmail.com>
2023-04-14 18:09:45 +0800

    Add new method and fix bug

--- src/session.jl
+++ src/session.jl
@@ -44,10 +44,10 @@ function new_session(::U2NetpModel)
     return SimpleSession("U2Netp", OX.load_inference(U2NETP_ONNX_PATH))
 end
 function new_session(::U2NetClothSegModel)
-    return SimpleSession("U2NetClothSeg", OX.load_inference(U2NET_CLOTH_SEG_ONNX_PATH))
+    return ClothSession("U2NetClothSeg", OX.load_inference(U2NET_CLOTH_SEG_ONNX_PATH))
 end
 function new_session(::U2NetHumanSegModel)
-    return ClothSession("U2NetHumanSeg", OX.load_inference(U2NET_CLOTH_SEG_ONNX_PATH))
+    return SimpleSession("U2NetHumanSeg", OX.load_inference(U2NET_HUMAN_SEG_ONNX_PATH))
 end
 function new_session(::SiluetaModel)
     return SimpleSession("Silueta", OX.load_inference(SILUETA_ONNX_PATH))
@@ -60,8 +60,8 @@ function predict(session::SimpleSession, img::Matrix{<:Colorant})
 
     # Preprocessing
     small_img = imresize(img, 320, 320)
-    img_array = collect(channelview(small_img))  # type: Array {N0f8, 3}, size: (channel(RGB), hight, width)
-    img_array = collect(rawview(img_array))  # type: Array {UInt8, 3}, size: (channel(RGB), hight, width)
+    img_array = copy(channelview(small_img))  # type: Array {N0f8, 3}, size: (channel(RGB), hight, width)
+    img_array = copy(rawview(img_array))  # type: Array {UInt8, 3}, size: (channel(RGB), hight, width)
     img_array = img_array / maximum(img_array)
     tmp_img = zeros(Float32, 1, 3, 320, 320)  # input size: (batch_size, 3, 320, 320)
     tmp_img[1, 1, :, :] = (img_array[1, :, :] .- 0.485) / 0.229
@@ -85,3 +85,80 @@ function predict(session::SimpleSession, img::Matrix{<:Colorant})
     mask = imresize(mask, size(img))
     return [mask]
 end
+
+function predict(session::DisSession, img::Matrix{<:Colorant})
+
+    # Preprocessing
+    small_img = imresize(img, 1024, 1024)
+    img_array = copy(channelview(small_img))  # type: Array {N0f8, 3}, size: (channel(RGB), hight, width)
+    img_array = copy(rawview(img_array))  # type: Array {UInt8, 3}, size: (channel(RGB), hight, width)
+    img_array = img_array / maximum(img_array)
+    tmp_img = zeros(Float32, 1, 3, 1024, 1024)  # input size: (batch_size, 3, 320, 320)
+    tmp_img[1, 1, :, :] = (img_array[1, :, :] .- 0.485) / 1.0
+    tmp_img[1, 2, :, :] = (img_array[2, :, :] .- 0.456) / 1.0
+    tmp_img[1, 3, :, :] = (img_array[3, :, :] .- 0.406) / 1.0
+
+    # Predict
+    input = Dict(session.onnx_session.input_names[1] => tmp_img)
+    # output size: (batch_size, 1, height, width)
+    pred = session.onnx_session(input)[session.onnx_session.output_names[1]][:, 1, :, :]
+
+    # Postprocessing
+    ma = maximum(pred)
+    mi = minimum(pred)
+    pred = (pred .- mi) / (ma .- mi)
+    pred = dropdims(pred; dims=1)
+
+    # Creates a view of the numeric array `pred`,
+    # interpreting successive elements of `pred` as if they were channels of Colorant `Gray`.
+    mask = colorview(Gray, pred)
+    mask = imresize(mask, size(img))
+    return [mask]
+end
+
+# TODO
+# function predict(session::ClothSession, img::Matrix{<:Colorant})
+
+#     # Preprocessing
+#     small_img = imresize(img, 768, 768)
+#     img_array = copy(channelview(small_img))  # type: Array {N0f8, 3}, size: (channel(RGB), hight, width)
+#     img_array = copy(rawview(img_array))  # type: Array {UInt8, 3}, size: (channel(RGB), hight, width)
+#     img_array = img_array / maximum(img_array)
+#     tmp_img = zeros(Float32, 1, 3, 768, 768)  # input size: (batch_size, 3, 768, 768)
+#     tmp_img[1, 1, :, :] = (img_array[1, :, :] .- 0.485) / 0.229
+#     tmp_img[1, 2, :, :] = (img_array[2, :, :] .- 0.456) / 0.224
+#     tmp_img[1, 3, :, :] = (img_array[3, :, :] .- 0.406) / 0.225
+
+#     # Predict
+#     input = Dict(session.onnx_session.input_names[1] => tmp_img)
+#     # output size: (batch_size, 1, height, width)
+#     pred = session.onnx_session(input)
+#     println(size(pred[session.onnx_session.output_names[1]]))
+#     pred = log_softmax(pred[session.onnx_session.output_names[1]], 1)
+#     # pred = argmax(pred; dims=1)
+#     println(size(pred))
+#     pred = dropdims(pred; dims=1)
+#     println(size(pred))
+
+#     mask = colorview(RGBA, pred)
+#     mask = imresize(mask, size(img))
+
+#     masks = []
+
+#     mask1 = copy(mask)
+#     # mask1.putpalette(pallete1)
+#     # mask1 = mask1.convert("RGB").convert("L")
+#     push!(masks, mask1)
+
+#     mask2 = copy(mask)
+#     # mask1.putpalette(pallete1)
+#     # mask1 = mask1.convert("RGB").convert("L")
+#     push!(masks, mask2)
+
+#     mask3 = copy(mask)
+#     # mask1.putpalette(pallete1)
+#     # mask1 = mask1.convert("RGB").convert("L")
+#     push!(masks, mask3)
+
+#     return masks
+# end
