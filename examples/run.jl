56966bece7753a0a039c661250a1112bc47295e4
kai <supon3060@gmail.com>
2023-04-21 16:23:30 +0800

    Update example

--- examples/run.jl
+++ examples/run.jl
@@ -1,28 +1,39 @@
 using RemBG
 using Images
 
-exampledir(args...) = joinpath(@__DIR__, args...);
-img = load(exampledir("animal-1.jpg"))::Matrix{<:Colorant};
-img_num = copy(channelview(img))::Array{<:Number};
+exampledir(args...) = abspath(joinpath(dirname(pathof(RemBG)), "..", "examples", args...))
 
-output_img_num = remove(img_num);
-output_img = remove(img);
+img_animal = load(exampledir("animal-1.jpg"))::Matrix{<:Colorant};
+img_animal_num = copy(channelview(img_animal))::Array{<:Number};
+img_cloth = load(exampledir("cloth-1.jpg"))::Matrix{<:Colorant};
+img_cloth_num = copy(channelview(img))::Array{<:Number};
 
-session = new_session(U2Net);
-output_img_num = remove(img_num; session=session);
-output_img = remove(img; session=session);
+output_img_num = remove(img_animal_num);
+output_img = remove(img_animal);  # If we do not pass an inference session to `remove`, it will use a new session containing the U2Net model.
 
-session = new_session(U2NetHumanSeg);
-output_img_num = remove(img_num; session=session);
-output_img = remove(img; session=session);
+# Use a specific model. The available models are U2Net, U2Netp, U2NetClothSeg, U2NetHumanSeg, Silueta, ISNetGeneralUse
+session = new_session(U2Netp)
+output_img_animal_num = remove(img_animal_num; session=session)
+output_img_animal = remove(img_animal; session=session)
 
-session = new_session(ISNetGeneralUse);
-output_img_num = remove(img_num; session=session);
-output_img = remove(img; session=session);
+session = new_session(U2NetClothSeg)
+output_img_cloth_num = remove(img_cloth_num; session=session)
+output_img_cloth = remove(img_cloth; session=session)
 
-# cloth
-img = load(exampledir("cloth-1.jpg"))::Matrix{<:Colorant};
-img_num = copy(channelview(img))::Array{<:Number};
-session = new_session(U2NetClothSeg);
-output_img_num = remove(img_num; session=session);
-output_img = remove(img; session=session);
+session = new_session(U2NetHumanSeg)
+output_img_animal_num = remove(img_animal_num; session=session)
+output_img_animal = remove(img_animal; session=session)
+
+session = new_session(Silueta)
+output_img_animal_num = remove(img_animal_num; session=session)
+output_img_animal = remove(img_animal; session=session)
+
+session = new_session(ISNetGeneralUse)
+output_img_animal_num = remove(img_animal_num; session=session)
+output_img_animal = remove(img_animal; session=session)
+
+# Return mask
+output_img_animal = remove(img_animal; only_mask=true)  # return mask
+
+# Apply background color
+output_img_animal = remove(img_animal; bgcolor=(255, 0, 0, 255))
